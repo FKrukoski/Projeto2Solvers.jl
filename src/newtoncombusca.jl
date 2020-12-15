ef2288161f8baf168c9dbd96a58d176f21a88866
Franklin Angelo Krukoski <franklin.krukoski@gmail.com>
2020-12-14 22:39:33 -0300

    Segunda Gloriosa 2

--- src/newtoncombusca.jl
+++ src/newtoncombusca.jl
@@ -11,81 +11,82 @@ Options:
 - max_time: maximum elapsed time in seconds, use ≤ 0 for unlimited (default: 10)
 
 Disclaimers for the developer:
-- nlp should be the only mandatory argument
-- these five options are the current default for other JSO-compliant solvers
-- always return a GenericExecutionStats
-"""
-function newtoncombusca(
-  nlp::AbstractNLPModel;
-  atol::Real = 1e-6,
-  rtol::Real = 1e-6,
-  max_eval::Int = 1000,
-  max_iter::Int = 0,
-  max_time::Float64 = 10.0
-)
-
-  if !unconstrained(nlp)
-    error("Problem is not unconstrained")
-  end
-
-  x = copy(nlp.meta.x0)
-
-  f(x) = obj(nlp, x)
-  ∇f(x) = grad(nlp, x)
-  H(x) = Symmetric(hess(nlp, x), :L)
-  fx = f(x)
-  ∇fx = ∇f(x)
- 
-  ϵ = atol + rtol * norm(∇fx)
-  t₀ = time()
-
-  x⁺ = similar(x) #estava faltando isso!
-
-  iter = 0
-  Δt = time() - t₀
-  solved = norm(∇fx) < ϵ # First order stationary
-  tired = neval_obj(nlp) ≥ max_eval > 0|| 
-          iter ≥ max_iter > 0 || Δt ≥ max_time > 0 # Excess time, iteration, evaluations
-  
-  α = 1.0
-  η = 1e-2
-  Β = 1.0e-3
-  
-  # status must be one of a few options found in SolverTools.show_statuses()
-  # A good default value is :unknown.
-  status = :unknown
-
-  # log_header is up for some rewrite in the future. For now, it simply prints the column names with some spacing
-  @info log_header(
+  - nlp should be the only mandatory argument
+  - these five options are the current default for other JSO-compliant solvers
+  - always return a GenericExecutionStats
+  """
+  function newtoncombusca(
+    nlp::AbstractNLPModel;
+    atol::Real = 1e-6,
+    rtol::Real = 1e-6,
+    max_eval::Int = 1000,
+    max_iter::Int = 0,
+    max_time::Float64 = 10.0
+    )
+    
+    if !unconstrained(nlp)
+      error("Problem is not unconstrained")
+    end
+    
+    x = copy(nlp.meta.x0)
+    
+    f(x) = obj(nlp, x)
+    ∇f(x) = grad(nlp, x)
+    H(x) = Symmetric(hess(nlp, x), :L)
+    fx = f(x)
+    ∇fx = ∇f(x)
+    
+    ϵ = atol + rtol * norm(∇fx)
+    t₀ = time()
+    
+    x⁺ = similar(x) #estava faltando isso!
+    
+    iter = 0
+    Δt = time() - t₀
+    solved = norm(∇fx) < ϵ # First order stationary
+    tired = neval_obj(nlp) ≥ max_eval > 0|| 
+    iter ≥ max_iter > 0 || Δt ≥ max_time > 0 # Excess time, iteration, evaluations
+    
+    α = 1.0
+    η = 1e-2
+    Β = 1.0e-3
+    
+    # status must be one of a few options found in SolverTools.show_statuses()
+    # A good default value is :unknown.
+    status = :unknown
+    
+    # log_header is up for some rewrite in the future. For now, it simply prints the column names with some spacing
+    @info log_header(
     [:iter, :fx, :ngx, :nf, :Δt],
     [Int, Float64, Float64, Int, Float64],
     hdr_override=Dict(:fx => "f(x)", :ngx => "‖∇f(x)‖", :nf => "#f")
-  )
-  # log_row uses the type information of each value, thus we use `Any` here.
-  @info log_row(
+    )
+    # log_row uses the type information of each value, thus we use `Any` here.
+    @info log_row(
     Any[iter, fx, norm(∇fx), neval_obj(nlp), Δt]
-  )
-
-  while !(solved || tired)
-    h = H(x)
-
-    F = tentacholesky(h, Β)
-        
-    if status != :unknown #small_step
-      break
-    end
+    )
     
-    J = F.L  
-    y = - J \ ∇fx
-    M = J'
-    d = M \ y
-    slope = dot(d, ∇fx)
-
-    # Armijo
-    α = 1.0
-    x⁺ = x + α * d
-    f⁺ = f(x⁺)
-    while f⁺ ≥ fx + η * α * slope
+    while !(solved || tired)
+      
+      if status != :unknown #small_step
+        break
+      end
+      
+      h = H(x)
+      F = tentacholesky(h, Β)
+      
+      # J = F.L  
+      # y = - J \ ∇fx
+      # M = J'
+      # d = M \ y
+      d = -(F \ ∇fx)
+      slope = dot(d, ∇fx)
+      
+      # Armijo
+      α = 1.0
+      x⁺ = x + α * d
+      f⁺ = f(x⁺)
+      while f⁺ ≥ fx + η * α * slope
         α = α / 2
         x⁺ = x + α * d
         f⁺ = f(x⁺)
@@ -93,39 +94,39 @@ function newtoncombusca(
           status = :small_step
           break
         end
-    end
-    if status != :unknown #small_step
-      break
-    end
-    x .= x⁺
-    fx = f⁺
-    ∇fx = ∇f(x)  
-    iter += 1
-
-    solved = norm(∇fx) < ϵ # First order stationary
-
-    Δt = time() - t₀
-    tired = neval_obj(nlp) ≥ max_eval > 0|| 
-            iter ≥ max_iter > 0 || Δt ≥ max_time > 0 # Excess time, iteration, evaluations
-
-    @info log_row(
+      end
+      if status != :unknown #small_step
+        break
+      end
+      x .= x⁺
+      fx = f⁺
+      ∇fx = ∇f(x)  
+      iter += 1
+      
+      solved = norm(∇fx) < ϵ # First order stationary
+      
+      Δt = time() - t₀
+      tired = neval_obj(nlp) ≥ max_eval > 0|| 
+      iter ≥ max_iter > 0 || Δt ≥ max_time > 0 # Excess time, iteration, evaluations
+      
+      @info log_row(
       Any[iter, fx, norm(∇fx), neval_obj(nlp), Δt]
-    )
-  end
-
-  if solved
-    status = :first_order
-  elseif tired
-    if neval_obj(nlp) ≥ max_eval > 0
-      status = :max_eval
-    elseif iter ≥ max_iter > 0
-      status = :max_iter
-    elseif Δt ≥ max_time > 0
-      status = :max_time
+      )
     end
-  end
-
-  return GenericExecutionStats(
+    
+    if solved
+      status = :first_order
+    elseif tired
+      if neval_obj(nlp) ≥ max_eval > 0
+        status = :max_eval
+      elseif iter ≥ max_iter > 0
+        status = :max_iter
+      elseif Δt ≥ max_time > 0
+        status = :max_time
+      end
+    end
+    
+    return GenericExecutionStats(
     status,
     nlp,
     solution=x,
@@ -133,32 +134,33 @@ function newtoncombusca(
     dual_feas=norm(∇fx),
     elapsed_time=Δt,
     iter=iter
-  )
-end
-
-function tentacholesky(h, Β)
-  F = copy(h)
-  fatorou=:inicio
-  if minimum(diag(h)) > 0     
-    τ = 0.0
-  else
-    τ = -minimum(diag(h)) + Β
-  end  
-  k = 0
-  while fatorou!=:fatorou
-    F = cholesky(h, check = false)
-    if !issuccess(F)
-      τ = max(2τ, Β)
-      h = h+τ*I
-      k +=1
-      if k>100
-        @error("muitas iterações")
-          break
-      end
-    else
-      fatorou =:fatorou
-    end
+    )
   end
-  return F
-end
-
+  
+  function tentacholesky(h, Β)
+    F = copy(h)
+    fatorou=:inicio
+    if minimum(diag(h)) > 0     
+      τ = 0.0
+    else
+      τ = -minimum(diag(h)) + Β
+    end  
+    k = 0
+    while fatorou!=:fatorou
+      F = cholesky(h, check = false)
+      if !issuccess(F)
+        τ = max(2τ, Β)
+        h = h+τ*I
+        k +=1
+        if k>100
+          @error("muitas iterações")
+          break
+        end
+      else
+        fatorou =:fatorou
+      end
+    end
+    return F
+  end
+  
+  
