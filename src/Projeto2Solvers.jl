e76be1d0ecc5cde25f0aba631d52e9cd6e25e662
Franklin Angelo Krukoski <franklin.krukoski@gmail.com>
2020-12-21 18:42:19 -0300

    com os 4

--- src/Projeto2Solvers.jl
+++ src/Projeto2Solvers.jl
@@ -2,6 +2,7 @@ module Projeto2Solvers
 
 # standard lib
 using LinearAlgebra
+using ForwardDiff
 
 # JSO packages
 using NLPModels
@@ -13,5 +14,8 @@ include("uncsolver.jl")
 include("newtoncombusca.jl")
 include("l_bfgs_rcst.jl")
 include("basicos.jl")
+include("StrongWolfe.jl")
+include("Newton_rc_bissec.jl")
+include("Lbfgs_StrongWolfe.jl")
 
 end # module
